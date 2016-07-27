0e2ecce178cec185dd756d6490e30ed14becbb4f
Jan Kruse <privat@jan-tristan.de>
2016-07-28 00:43:42 +0200

    Added gitignore file and the possibility to add an amount of random cells. Cells are not counted the right way.

--- src/gamefield.c
+++ src/gamefield.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct {
 	int **field;
@@ -44,23 +45,43 @@ void destroy_gameField(gameField *gF ) {
 }
 
 void print_gameField( gameField *gF ) {
+	for( int i=0; i<gF->width+2; i++) {
+		printf("-");
+	}
 	for( int z=0; z<gF->height; z++) {
+		printf("\n|");
 		for( int s=0; s<gF->width; s++) {
-			printf( "%d ", gF->field[z][s] );
+			(gF->field[z][s] == 0) ? printf(" ") : printf("#");
 		}
-		printf("\n");
+		printf("|");
+	}
+	printf("\n");
+	for( int i=0; i<gF->width+2; i++) {
+		printf("-");
 	}
 }
 
-/*int count_neighbours(gameField *gf, int z, int s) {
+int count_neighbours(gameField *gf, int z, int s) {
 	int count = 0;
-	for ( z<=0 ? int bZ=0 : int bZ=z-1; z<gf->height ? bZ<z : bZ<=z+1; bZ++) {
-		for (s<=0 ? int bS = 0 : int bS=s-1; z<gf->width ? bS<s : bS<=s+1; bS++) {
+	int bZ=0;
+	if( z>0 )
+		bZ=z-1;
+	for ( ; bZ<=z+1 && bZ<gf->height; bZ++) {
+		int bS=0;
+		if( s>0 )
+			bS=s-1;
+		for ( ; bS<=s+1 && bS<gf->width; bS++) {
 			if (gf->field[bZ][bS] == 1)
 				count++;
 		}
 	}
-	return count;
+	if( count < 0 ) {
+		return 0;
+	} else if( gf->field[z][s] == 1 ) {
+		return count-1;
+	} else {
+		return count;
+	}
 }
 
 gameField *calc_new_state(gameField *gf) {
@@ -79,16 +100,26 @@ gameField *calc_new_state(gameField *gf) {
 	}
 
 	return newState;
-}*/
+}
+
+void set_random_cells(gameField *gf, int n) {
+	for( int i=0; i<n; i++ ) {
+		set_cell(gf, rand()%gf->width, rand()%gf->height, 1);
+	}
+}
 
 int main() {
+	srand(time(NULL));
 	gameField *field = make_gameField(10, 10);
-	set_cell(field, 3, 3, 1);
+	/*set_cell(field, 3, 3, 1);
 	set_cell(field, 4, 3, 1);
-	set_cell(field, 5, 3, 1);
-	set_cell(field, 5, 4, 1);
+	set_cell(field, 5, 3, 1);*/
+	set_random_cells(field, 20);
 	print_gameField(field);
-	//field = calc_new_state(field);
-	print_gameField(field);
+	for( int i=0; i<5; i++ ) {
+		printf("\n");
+		field = calc_new_state(field);
+		print_gameField(field);
+	}
 	destroy_gameField(field);
 }
